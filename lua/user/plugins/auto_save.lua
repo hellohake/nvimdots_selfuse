928bbfb4d14ffd39043a4019bb9f4b8de081756d
lihao.hellohake <lihao.hellohake@bytedance.com>
2025-05-25 20:49:00 +0800

    update

--- lua/user/plugins/auto_save.lua
+++ lua/user/plugins/auto_save.lua
@@ -2,6 +2,7 @@ local custom = {}
 
 -- https://github.com/okuuva/auto-save.nvim
 custom["okuuva/auto-save.nvim"] = {
+	enabled = false,
 	"okuuva/auto-save.nvim",
 	version = "^1.0.0", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
 	cmd = "ASToggle", -- optional for lazy loading on command
